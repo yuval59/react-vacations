667ee5f59df7018ad7dce1db7f5a3400b1344c13
yuval59 <yuvalmaron222@gmail.com>
2023-06-07 10:51:47 +0300

    Added Zod config

---
+++ src/server/config.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envVariables = z.object({
+  DB_HOST: z.string(),
+  DB_PORT: z.string(),
+  DB_USER: z.string(),
+  DB_PASS: z.string(),
+  DB_DATABASE: z.string(),
+})
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envVariables> {}
+  }
+}
+
+const config = envVariables.parse(process.env)
+
+export default config
