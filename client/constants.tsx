6fe935cc3c67d458028f5f85ff96e7aefd62f08c
yuval59 <yuvalmaron222@gmail.com>
2023-06-18 19:36:34 +0300

    Changed base server url to environment variable

--- client/constants.tsx
+++ client/constants.tsx
@@ -1,5 +1,18 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envVariables = z.object({
+  SERVER_BASE: z.string(),
+})
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envVariables> {}
+  }
+}
+
 export const FETCH_ROUTES = {
-  BASE: 'http://localhost:3030',
+  BASE: envVariables.parse(process.env).SERVER_BASE,
   VACATIONS: '/vacations',
   LOGIN: '/login',
   REGISTER: '/register',
